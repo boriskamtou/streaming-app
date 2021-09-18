a0c9f7eff5824573d54ad6800d5a000efc153414
kamtou boris <boriskamtou@gmail.com>
2021-09-18 22:26:09 +0200

    add dependencies and auth architecture

---
+++ lib/core/presentation/app_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Streaming - app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
